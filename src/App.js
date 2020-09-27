860eebc82884849e04a764ced4af83e25c47f9a0
cruceruvlad <71463907+cruceruvlad@users.noreply.github.com>
2020-09-27 22:21:53 +0300

    User Profile Update

--- src/App.js
+++ src/App.js
@@ -87,7 +87,17 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
-      .then(response => this.calculateFaceLocation(response))
+      .then(response => {
+        if(response)
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({id:this.state.user.id})
+        }).then(res => res.json()).then(count => {
+          this.setState(Object.assign(this.state.user,{entries: count}))
+        })
+        this.calculateFaceLocation(response);
+      })
       .catch(err => console.log(err));
   }
 
@@ -102,13 +112,13 @@ class App extends Component {
         { route === 'home'
             ? <div>
                 <Logo />
-                <Rank />
+                <Rank name={this.state.user.name} entries={this.state.user.entries}/>
                 <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
                 <FaceRecognition imageURL={imageURL} box={box}/>
               </div>
             : (
                 route === 'signin'
-                ? <Signin onRouteChange={this.onRouteChange}/>
+                ? <Signin onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
                 : <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
             )
         }
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -22,9 +22,11 @@ class Signin extends Component {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({email:this.state.signInEmail, password: this.state.signInPassword})
-        }).then(res => res.json()).then(data => {
-            if(data === 'success')
+        }).then(res => res.json()).then(user => {
+            if(user.id){
+                this.props.loadUser(user);
                 this.props.onRouteChange('home');
+            }
         });
     }
 
